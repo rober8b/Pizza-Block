1d0e4541c6d9315d535dbed1d19b86ea1b73ee77
Robertino <robertinobarbuto@gmail.com>
2023-09-06 13:25:32 -0300

    actu-precios-septiembre-2023

--- src/data/Papas-data.jsx
+++ src/data/Papas-data.jsx
@@ -1,7 +1,7 @@
 const papasData = [
-    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 1250 },
-    { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 900 },
-    { id: 3, nombre: 'Provenzal', precio: 750 },
+    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 1500 },
+    { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 1200 },
+    { id: 3, nombre: 'Provenzal', precio: 950 },
 ]
 
 export default papasData
--- src/data/Sandwiches-data.jsx
+++ src/data/Sandwiches-data.jsx
@@ -1,7 +1,7 @@
 const sandwichesData = [
-    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 2000 },
-    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 2500 },
-    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 3000 },
+    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 2500 },
+    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 3000 },
+    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 3500 },
 ]
 
 export default sandwichesData
