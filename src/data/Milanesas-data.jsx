af86caa1d5bda1fa2bfe1254fa0c9d8f21916ca5
Robertino <robertinobarbuto@gmail.com>
2023-12-07 13:11:42 -0300

    precios diciembre

--- src/data/Milanesas-data.jsx
+++ src/data/Milanesas-data.jsx
@@ -1,20 +1,20 @@
 
 const milanesasData = [
-    { id: 1, nombre: 'Milanesa', descripcion: 'Milanesas + papas fritas', precio: 6500 },
-    { id: 2, nombre: 'Napolitana', descripcion: 'Salsa de tomate, mozzarella y tomate natural + fritas',precio: 8500 },
-    { id: 3, nombre: 'Jamón y Morrón', descripcion: 'Salsa de tomate, mozzarella , jamón y morrón',precio: 9500 },
-    { id: 4, nombre: 'Jamón y Huevo', descripcion: 'Salsa de tomate, mozzarella, jamón y huevo duro + fritas', precio: 9500 },
-    { id: 5, nombre: 'Fugazza c/Queso', descripcion: 'Mozzarella y cebolla + fritas',precio: 8000 },
-    { id: 6, nombre: 'Primavera', descripcion: 'Salsa de tomate, mozzarella, tomate, huevo y jamón + fritas',precio: 10500 },
-    { id: 7, nombre: 'Provolone', descripcion: 'Salsa de tomate, mozzarella y provolone + fritas',precio: 9500 },
-    { id: 8, nombre: 'Roquefort', descripcion: 'Salsa de tomate, mozzarella y roquefort + fritas',precio: 9500 },
-    { id: 9, nombre: 'Hawaiana', descripcion: 'Salsa de tomate, mozzarella, jamón y ananá + fritas',precio: 10500 },
-    { id: 10, nombre: 'Calabresa', descripcion: 'Salsa de tomate, mozzarella y calabresa + fritas',precio: 9500 },
-    { id: 11, nombre: 'Rúcula y Jamón Crudo', descripcion: 'Salsa de tomate, mozzarella, rúcula natural  y jamón crudo + fritas',precio: 10500 },
-    { id: 12, nombre: 'Rúcula y Parmesano', descripcion: 'Salsa de tomate, mozzarella, rúcula natural y parmesano + fritas',precio: 10500 },
-    { id: 13, nombre: 'Mila BLOCK', descripcion: 'Salsa de tomate, mozzarella, panceta, huevos fritos y papas fritas + porción de fritas',precio: 12000 },
-    { id: 14, nombre: 'Porción de Fritas', descripcion: '(Para Compartir)',precio: 4000 },
-    { id: 15, nombre: 'Fritas', descripcion: '(Media Porción)',precio: 2500 }
+    { id: 1, nombre: 'Milanesa', descripcion: 'Milanesas + papas fritas', precio: 7500 },
+    { id: 2, nombre: 'Napolitana', descripcion: 'Salsa de tomate, mozzarella y tomate natural + fritas',precio: 9500 },
+    { id: 3, nombre: 'Jamón y Morrón', descripcion: 'Salsa de tomate, mozzarella , jamón y morrón',precio: 10500 },
+    { id: 4, nombre: 'Jamón y Huevo', descripcion: 'Salsa de tomate, mozzarella, jamón y huevo duro + fritas', precio: 10500 },
+    { id: 5, nombre: 'Fugazza c/Queso', descripcion: 'Mozzarella y cebolla + fritas',precio: 9000 },
+    { id: 6, nombre: 'Primavera', descripcion: 'Salsa de tomate, mozzarella, tomate, huevo y jamón + fritas',precio: 12000 },
+    { id: 7, nombre: 'Provolone', descripcion: 'Salsa de tomate, mozzarella y provolone + fritas',precio: 10500 },
+    { id: 8, nombre: 'Roquefort', descripcion: 'Salsa de tomate, mozzarella y roquefort + fritas',precio: 10500 },
+    { id: 9, nombre: 'Hawaiana', descripcion: 'Salsa de tomate, mozzarella, jamón y ananá + fritas',precio: 12000 },
+    { id: 10, nombre: 'Calabresa', descripcion: 'Salsa de tomate, mozzarella y calabresa + fritas',precio: 10500 },
+    { id: 11, nombre: 'Rúcula y Jamón Crudo', descripcion: 'Salsa de tomate, mozzarella, rúcula natural  y jamón crudo + fritas',precio: 12000 },
+    { id: 12, nombre: 'Rúcula y Parmesano', descripcion: 'Salsa de tomate, mozzarella, rúcula natural y parmesano + fritas',precio: 11000 },
+    { id: 13, nombre: 'Mila BLOCK', descripcion: 'Salsa de tomate, mozzarella, panceta, huevos fritos y papas fritas + porción de fritas',precio: 14500 },
+    { id: 14, nombre: 'Porción de Fritas', descripcion: '(Para Compartir)',precio: 4500 },
+    { id: 15, nombre: 'Fritas', descripcion: '(Media Porción)',precio: 3000 }
 
 ]
 
--- src/data/Sandwiches-data.jsx
+++ src/data/Sandwiches-data.jsx
@@ -1,7 +1,7 @@
 const sandwichesData = [
-    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 3000 },
-    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 3500 },
-    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 4000 },
+    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 3500 },
+    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 4000 },
+    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 4500 },
 ]
 
 export default sandwichesData
