4d5b28e3475e026c7dc001c8cd7544f081da1b56
Robertino <robertinobarbuto@gmail.com>
2023-12-21 13:54:24 -0300

    actualizacion diciembre 21

--- src/data/Papas-data.jsx
+++ src/data/Papas-data.jsx
@@ -1,7 +1,7 @@
 const papasData = [
-    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 2000 },
-    { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 1500 },
-    { id: 3, nombre: 'Provenzal', precio: 1200 },
+    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 2500 },
+    { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 1800 },
+    { id: 3, nombre: 'Provenzal', precio: 1500 },
 ]
 
 export default papasData
--- src/data/Sandwiches-data.jsx
+++ src/data/Sandwiches-data.jsx
@@ -1,7 +1,7 @@
 const sandwichesData = [
-    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 3500 },
-    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 4000 },
-    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 4500 },
+    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 4200 },
+    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 4800 },
+    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 5400 },
 ]
 
 export default sandwichesData
