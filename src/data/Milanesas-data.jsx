4964c4af9a2c7e091b6e118c1746c3aa3fbebcfb
Robertino <robertinobarbuto@gmail.com>
2024-03-18 15:01:22 -0300

    marzo 2024

--- src/data/Milanesas-data.jsx
+++ src/data/Milanesas-data.jsx
@@ -1,20 +1,20 @@
 
 const milanesasData = [
-    { id: 1, nombre: 'Milanesa', descripcion: 'Milanesas + papas fritas', precio: 9000 },
-    { id: 2, nombre: 'Napolitana', descripcion: 'Salsa de tomate, mozzarella y tomate natural + fritas',precio: 11500 },
-    { id: 3, nombre: 'Jamón y Morrón', descripcion: 'Salsa de tomate, mozzarella , jamón y morrón',precio: 12600 },
-    { id: 4, nombre: 'Jamón y Huevo', descripcion: 'Salsa de tomate, mozzarella, jamón y huevo duro + fritas', precio: 12600 },
-    { id: 5, nombre: 'Fugazza c/Queso', descripcion: 'Mozzarella y cebolla + fritas',precio: 108000 },
-    { id: 6, nombre: 'Primavera', descripcion: 'Salsa de tomate, mozzarella, tomate, huevo y jamón + fritas',precio: 14400 },
-    { id: 7, nombre: 'Provolone', descripcion: 'Salsa de tomate, mozzarella y provolone + fritas',precio: 12600 },
-    { id: 8, nombre: 'Roquefort', descripcion: 'Salsa de tomate, mozzarella y roquefort + fritas',precio: 12600 },
-    { id: 9, nombre: 'Hawaiana', descripcion: 'Salsa de tomate, mozzarella, jamón y ananá + fritas',precio: 14400 },
-    { id: 10, nombre: 'Calabresa', descripcion: 'Salsa de tomate, mozzarella y calabresa + fritas',precio: 12600 },
-    { id: 11, nombre: 'Rúcula y Jamón Crudo', descripcion: 'Salsa de tomate, mozzarella, rúcula natural  y jamón crudo + fritas',precio: 14400 },
-    { id: 12, nombre: 'Rúcula y Parmesano', descripcion: 'Salsa de tomate, mozzarella, rúcula natural y parmesano + fritas',precio: 13200 },
-    { id: 13, nombre: 'Mila BLOCK', descripcion: 'Salsa de tomate, mozzarella, panceta, huevos fritos y papas fritas + porción de fritas',precio: 17400 },
-    { id: 14, nombre: 'Porción de Fritas', descripcion: '(Para Compartir)',precio: 5400 },
-    { id: 15, nombre: 'Fritas', descripcion: '(Media Porción)',precio: 3600 }
+    { id: 1, nombre: 'Milanesa', descripcion: 'Milanesas + papas fritas', precio: 10000 },
+    { id: 2, nombre: 'Napolitana', descripcion: 'Salsa de tomate, mozzarella y tomate natural + fritas',precio: 12500 },
+    { id: 3, nombre: 'Jamón y Morrón', descripcion: 'Salsa de tomate, mozzarella , jamón y morrón',precio: 14500 },
+    { id: 4, nombre: 'Jamón y Huevo', descripcion: 'Salsa de tomate, mozzarella, jamón y huevo duro + fritas', precio: 14500 },
+    { id: 5, nombre: 'Fugazza c/Queso', descripcion: 'Mozzarella y cebolla + fritas',precio: 12000 },
+    { id: 6, nombre: 'Primavera', descripcion: 'Salsa de tomate, mozzarella, tomate, huevo y jamón + fritas',precio: 15500 },
+    { id: 7, nombre: 'Provolone', descripcion: 'Salsa de tomate, mozzarella y provolone + fritas',precio: 14500 },
+    { id: 8, nombre: 'Roquefort', descripcion: 'Salsa de tomate, mozzarella y roquefort + fritas',precio: 13500 },
+    { id: 9, nombre: 'Hawaiana', descripcion: 'Salsa de tomate, mozzarella, jamón y ananá + fritas',precio: 15500 },
+    { id: 10, nombre: 'Calabresa', descripcion: 'Salsa de tomate, mozzarella y calabresa + fritas',precio: 13500 },
+    { id: 11, nombre: 'Rúcula y Jamón Crudo', descripcion: 'Salsa de tomate, mozzarella, rúcula natural  y jamón crudo + fritas',precio: 15500 },
+    { id: 12, nombre: 'Rúcula y Parmesano', descripcion: 'Salsa de tomate, mozzarella, rúcula natural y parmesano + fritas',precio: 14500 },
+    { id: 13, nombre: 'Mila BLOCK', descripcion: 'Salsa de tomate, mozzarella, panceta, huevos fritos y papas fritas + porción de fritas',precio: 18500 },
+    { id: 14, nombre: 'Porción de Fritas', descripcion: '(Para Compartir)',precio: 5500 },
+    { id: 15, nombre: 'Fritas', descripcion: '(Media Porción)',precio: 4000 }
 
 ]
 
