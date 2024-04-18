dda6ae82220af269a5f64748cf6e6e58c72f9187
Robertino <robertinobarbuto@gmail.com>
2024-04-18 19:27:11 -0300

    18-04-2024

--- src/data/Sandwiches-data.jsx
+++ src/data/Sandwiches-data.jsx
@@ -1,7 +1,7 @@
 const sandwichesData = [
-    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 4500 },
-    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 5500 },
-    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 6500 },
+    { id: 1, nombre: 'Sándwich de Milanesa simple', descripcion: '', precio: 6000 },
+    { id: 2, nombre: 'Sándwich de Milanesa', descripcion: '(2 ingredientes a elección) Lechuga - tomate - jamón y queso',precio: 7000 },
+    { id: 3, nombre: 'Sándwich de Milanesa', descripcion: 'Completo',precio: 8000 },
 ]
 
 export default sandwichesData
