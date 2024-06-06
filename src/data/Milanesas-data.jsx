2bae26027f0aad3f3f590803b40a01093b7f5c7a
Robertino <robertinobarbuto@gmail.com>
2024-06-06 18:20:57 -0300

    actu junio

--- src/data/Milanesas-data.jsx
+++ src/data/Milanesas-data.jsx
@@ -1,20 +1,20 @@
 
 const milanesasData = [
-    { id: 1, nombre: 'Milanesa', descripcion: 'Milanesas + papas fritas', precio: 12000 },
-    { id: 2, nombre: 'Napolitana', descripcion: 'Salsa de tomate, mozzarella y tomate natural + fritas',precio: 15000 },
-    { id: 3, nombre: 'Jamón y Morrón', descripcion: 'Salsa de tomate, mozzarella , jamón y morrón',precio: 17500 },
-    { id: 4, nombre: 'Jamón y Huevo', descripcion: 'Salsa de tomate, mozzarella, jamón y huevo duro + fritas', precio: 17500 },
-    { id: 5, nombre: 'Fugazza c/Queso', descripcion: 'Mozzarella y cebolla + fritas',precio: 15000 },
-    { id: 6, nombre: 'Primavera', descripcion: 'Salsa de tomate, mozzarella, tomate, huevo y jamón + fritas',precio: 18500 },
-    { id: 7, nombre: 'Provolone', descripcion: 'Salsa de tomate, mozzarella y provolone + fritas',precio: 16500 },
-    { id: 8, nombre: 'Roquefort', descripcion: 'Salsa de tomate, mozzarella y roquefort + fritas',precio: 16500 },
-    { id: 9, nombre: 'Hawaiana', descripcion: 'Salsa de tomate, mozzarella, jamón y ananá + fritas',precio: 18500 },
-    { id: 10, nombre: 'Calabresa', descripcion: 'Salsa de tomate, mozzarella y calabresa + fritas',precio: 16500 },
-    { id: 11, nombre: 'Rúcula y Jamón Crudo', descripcion: 'Salsa de tomate, mozzarella, rúcula natural  y jamón crudo + fritas',precio: 18500 },
-    { id: 12, nombre: 'Rúcula y Parmesano', descripcion: 'Salsa de tomate, mozzarella, rúcula natural y parmesano + fritas',precio: 17500 },
-    { id: 13, nombre: 'Mila BLOCK', descripcion: 'Salsa de tomate, mozzarella, panceta, huevos fritos y papas fritas + porción de fritas',precio: 20500 },
-    { id: 14, nombre: 'Porción de Fritas', descripcion: '(Para Compartir)',precio: 6500 },
-    { id: 15, nombre: 'Fritas', descripcion: '(Media Porción)',precio: 5000 }
+    { id: 1, nombre: 'Milanesa', descripcion: 'Milanesas + papas fritas', precio: 15000 },
+    { id: 2, nombre: 'Napolitana', descripcion: 'Salsa de tomate, mozzarella y tomate natural + fritas',precio: 19500 },
+    { id: 3, nombre: 'Jamón y Morrón', descripcion: 'Salsa de tomate, mozzarella , jamón y morrón',precio: 21500 },
+    { id: 4, nombre: 'Jamón y Huevo', descripcion: 'Salsa de tomate, mozzarella, jamón y huevo duro + fritas', precio: 21500 },
+    { id: 5, nombre: 'Fugazza c/Queso', descripcion: 'Mozzarella y cebolla + fritas',precio: 18500 },
+    { id: 6, nombre: 'Primavera', descripcion: 'Salsa de tomate, mozzarella, tomate, huevo y jamón + fritas',precio: 23500 },
+    { id: 7, nombre: 'Provolone', descripcion: 'Salsa de tomate, mozzarella y provolone + fritas',precio: 21500 },
+    { id: 8, nombre: 'Roquefort', descripcion: 'Salsa de tomate, mozzarella y roquefort + fritas',precio: 21500 },
+    { id: 9, nombre: 'Hawaiana', descripcion: 'Salsa de tomate, mozzarella, jamón y ananá + fritas',precio: 24500 },
+    { id: 10, nombre: 'Calabresa', descripcion: 'Salsa de tomate, mozzarella y calabresa + fritas',precio: 21500 },
+    { id: 11, nombre: 'Rúcula y Jamón Crudo', descripcion: 'Salsa de tomate, mozzarella, rúcula natural  y jamón crudo + fritas',precio: 24500 },
+    { id: 12, nombre: 'Rúcula y Parmesano', descripcion: 'Salsa de tomate, mozzarella, rúcula natural y parmesano + fritas',precio: 24500 },
+    { id: 13, nombre: 'Mila BLOCK', descripcion: 'Salsa de tomate, mozzarella, panceta, huevos fritos y papas fritas + porción de fritas',precio: 26500 },
+    { id: 14, nombre: 'Porción de Fritas', descripcion: '(Para Compartir)',precio: 8000 },
+    { id: 15, nombre: 'Fritas', descripcion: '(Media Porción)',precio: 6500 }
 
 ]
 
--- src/data/Papas-data.jsx
+++ src/data/Papas-data.jsx
@@ -1,7 +1,7 @@
 const papasData = [
-    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 3500 },
-    { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 2500 },
-    { id: 3, nombre: 'Provenzal', precio: 2000 },
+    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 4500 },
+    { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 3500 },
+    { id: 3, nombre: 'Provenzal', precio: 3000 },
 ]
 
 export default papasData
