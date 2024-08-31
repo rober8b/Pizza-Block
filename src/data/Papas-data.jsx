1991a4344b7cf8057f20704a6f9336cdaa87cd38
Robertino <rober8b>
2024-08-31 16:03:57 -0300

    actualizacion septiembre

--- src/data/Papas-data.jsx
+++ src/data/Papas-data.jsx
@@ -1,5 +1,5 @@
 const papasData = [
-    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 4500 },
+    { id: 1, nombre: 'Cheddar - Panceta y Verdeo', precio: 5000 },
     { id: 2, nombre: 'Huevos Fritos (Dos)', precio: 3500 },
     { id: 3, nombre: 'Provenzal', precio: 3000 },
 ]
