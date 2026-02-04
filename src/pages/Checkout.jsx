fa3da63076b55acc59e5fea5be79f875eee05f3a
Robertino <robertinobarbuto@gmail.com>
2026-02-04 11:25:19 -0300

    ip actualizada

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -133,7 +133,7 @@ const Checkout = () => {
         envio: formData.tipoEntrega === 'delivery' ? COSTO_ENVIO : 0
       }
 
-      const response = await fetch('http://localhost:5000/api/pedido', {
+      const response = await fetch('http://159.89.140.82:8080/api/pedido', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
