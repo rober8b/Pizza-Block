ec1d42e88f1316138960a5f25cdf79cebe142381
Robertino <robertinobarbuto@gmail.com>
2026-02-05 10:24:13 -0300

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -133,7 +133,7 @@ const Checkout = () => {
         envio: formData.tipoEntrega === 'delivery' ? COSTO_ENVIO : 0
       }
 
-      const response = await fetch('https://mold-byte-beverages-caps.trycloudflare.com/api/pedido', {
+      const response = await fetch('https://stayed-conversion-divine-offers.trycloudflare.com/api/pedido', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
