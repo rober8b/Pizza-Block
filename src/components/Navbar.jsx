93819e5e95bf39f7c46b44534489879a1107f352
Robertino <robertinobarbuto@gmail.com>
2026-02-13 22:45:12 -0300

    Eliminar banner de comisión por plataforma de pago

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,6 @@
 import './Navbar.css'
 import logo from '../assets/logo.png';
 import React, { useEffect, useState } from 'react';
-import mpBanner from '../assets/MP-5.png';
-
 
 function Navbar() {
   const [isSticky, setIsSticky] = useState(false);
@@ -25,8 +23,6 @@ function Navbar() {
         <div className="header">
             <div className="logo-container">
                  <img src={logo} alt="Logo Pizza Block" />
-
-                 <img src={mpBanner} alt="5% Extra Mercado Pago" className="mp-banner-fixed" />
             </div>
           <div className="navbar">
             <nav className={isSticky ? 'navbar_nav sticky' : 'navbar_nav'}>
